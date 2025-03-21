c103c443e3ca495653276e4311907e5f494211ce
Liron Shriki <liron757@gmail.com>
2025-03-21 16:51:31 +0200

    Implemented logic to SignUpFragment

--- app/src/main/java/com/example/mycloset/consts/enums.kt
+++ app/src/main/java/com/example/mycloset/consts/enums.kt
@@ -1,5 +1,10 @@
 package com.example.mycloset.consts
 
+enum class LoadingState {
+    LOADING,
+    NOT_LOADING
+}
+
 interface Listener<T> {
     fun onComplete(data: T)
 }
--- app/src/main/java/com/example/mycloset/fragments/login/SignUpFragment.kt
+++ app/src/main/java/com/example/mycloset/fragments/login/SignUpFragment.kt
@@ -2,18 +2,29 @@ package com.example.mycloset.fragments.login
 
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.example.mycloset.activities.MainActivity
+import com.example.mycloset.adapters.SpinnerAdapter
+import com.example.mycloset.api.controllers.AuthController
+import com.example.mycloset.api.controllers.UserController
+import com.example.mycloset.api.firebase.FirebaseModel
+import com.example.mycloset.api.models.user.User
+import com.example.mycloset.consts.Listener
 import com.example.mycloset.databinding.FragmentSignUpBinding
 import com.example.mycloset.dialogs.LoadingDialog
+import java.util.UUID
 import java.util.regex.Pattern
 
 class SignUpFragment : Fragment() {
@@ -84,7 +95,15 @@ class SignUpFragment : Fragment() {
                 override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
 
-        //TODO(ADD REST CALL)
+        context?.let {
+            SpinnerAdapter.setCountrySpinner(
+                it, object : Listener<ArrayAdapter<String>> {
+                    override fun onComplete(data: ArrayAdapter<String>) {
+                        binding.signupCountrySpinner.adapter = data
+                    }
+                }
+            )
+        }
 
         fun isValidEmail(email: String): Boolean {
             val regex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$"
@@ -121,8 +140,53 @@ class SignUpFragment : Fragment() {
 
             if (validateInput()) {
                 loadingDialog.show()
-                //TODO(ADD SIGNUP LOGIC)
-                loadingDialog.cancel()
+
+                UserController.instance.isUsernameTaken(
+                    username, object : Listener<Boolean> {
+                        override fun onComplete(data: Boolean) {
+                            if (data) {
+                                makeAToast("Username is already taken")
+                            } else {
+                                UserController.instance.isEmailTaken(
+                                    email, object : Listener<Boolean> {
+                                        override fun onComplete(data: Boolean) {
+                                            if (data) {
+                                                makeAToast("Email is already taken")
+                                            } else {
+                                                val newUser = User(
+                                                    username,
+                                                    "",
+                                                    email,
+                                                    country,
+                                                    ArrayList()
+                                                )
+
+                                                if (isAvatarSelected) {
+                                                    val bitmap =
+                                                        (binding.signupAvatar.drawable as BitmapDrawable).bitmap
+                                                    val id = UUID.randomUUID().toString()
+
+                                                    FirebaseModel.instance.uploadImage(
+                                                        id,
+                                                        bitmap,
+                                                        object : Listener<String?> {
+                                                            override fun onComplete(data: String?) {
+                                                                newUser.avatar = data ?: ""
+                                                                signUp(newUser)
+                                                            }
+                                                        }
+                                                    )
+                                                } else {
+                                                    signUp(newUser)
+                                                }
+                                            }
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    }
+                )
             }
         }
 
@@ -135,6 +199,23 @@ class SignUpFragment : Fragment() {
         }
 
         return view
+    }
+
+    fun signUp (newUser: User) {
+        AuthController.instance.signUp(
+            newUser,
+            password,
+            object : Listener<Void?> {
+                override fun onComplete(data: Void?) {
+                    val intent = Intent(
+                        activity,
+                        MainActivity::class.java
+                    )
+                    startActivity(intent)
+                    activity?.finish()
+                }
+            }
+        )
     }
 
     override fun onDestroy() {
